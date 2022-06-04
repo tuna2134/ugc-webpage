f57e4a3e335351ea9642b2ea0f80216caadd7fdf
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-06-04 23:26:42 +0900

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -1,1 +1,11 @@
-export default function Layout()
+import Head from 'next/head'
+
+export default function Layout({ children }) {
+  return (
+    <div>
+      <Head>
+        <title>union global chat公式サイト</title>
+      </Head>
+    </div>
+  )
+}
