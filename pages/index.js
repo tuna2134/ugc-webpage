c3914d2ec71015c8957c268c0f1540bc9af2ba38
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-06-05 06:56:30 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,9 @@ import Layout from "../components/layout"
 export default function Home() {
   return (
     <Layout>
-      <p>hello</p>
+      <h2 className="text-3xl">About</h2>
+      <p>Union Global Chatはbotがグローバルチャットを共有する感じです。</p>
+      <p>導入数が少なくても、他のbotがいるから安心</p>
     </Layout>
   )
 }
