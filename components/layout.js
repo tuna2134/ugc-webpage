4359b38a334b314054ab34a9aef76ad685015678
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-06-05 06:45:20 +0900

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -6,6 +6,10 @@ export default function Layout({ children }) {
       <Head>
         <title>union global chat公式サイト</title>
       </Head>
+      <div className="flex justify-between">
+        <h1>UGC</h1>
+        <a href="">support</a>
+      </div>
     </div>
   )
 }
