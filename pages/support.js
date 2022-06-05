f469ec87faf948a52b6b7c5f0a75d17680e498b2
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-06-05 21:00:17 +0900

    Create support.js

---
+++ pages/support.js
@@ -0,0 +1,9 @@
+import Layout import '../components/layout'
+
+export default function Support() {
+  return (
+    <Layout>
+      <a href="https://discord.gg/EVXYfrgyqh">Discord</a>
+    </Layout>
+  )
+}
