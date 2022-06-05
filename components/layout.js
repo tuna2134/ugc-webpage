d76f80331a902fc634724b5a39619ff1830a4a38
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-06-05 21:08:12 +0900

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -10,7 +10,9 @@ export default function Layout({ children }) {
       <nav className="bg-violet-500 flex justify-between h-10">
         <h1 className="text-4xl text-white">UGC</h1>
         <div>
-          <Link href="/support" className="rounded-md text-2xl text-white bg-violet-300">support</Link>
+          <Link href="/support">
+            <a className="rounded-md text-2xl text-white bg-violet-300">support</a>
+          </Link>
         </div>
       </nav>
       <main className="h-5">{children}</main>
