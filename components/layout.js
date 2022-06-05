ec0e7f27d364a53fc5796499a4c4fef1eb2ef151
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-06-05 21:24:24 +0900

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -8,7 +8,9 @@ export default function Layout({ children }) {
         <title>union global chat公式サイト</title>
       </Head>
       <nav className="bg-violet-500 flex justify-between h-14">
-        <h1 className="text-4xl text-white">UGC</h1>
+        <Link href="/">
+          <h1 className="text-4xl text-white">UGC</h1>
+        </Link>
         <div>
           <Link href="/support">
             <a className="rounded-md text-2xl text-white bg-violet-300">support</a>
