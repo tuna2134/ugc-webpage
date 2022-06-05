ef16dc0aa6496960dcf2a538791d24dd53f3a367
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-06-05 21:14:04 +0900

    Update support.js

--- pages/support.js
+++ pages/support.js
@@ -3,9 +3,11 @@ import Layout from '../components/layout'
 export default function Support() {
   return (
     <Layout>
-      <div className="border-4 rounded-md">
-        <h3>Discord</h3>
-        <a href="https://discord.gg/EVXYfrgyqh">https://discord.gg/EVXYfrgyqh</a>
+      <div className="flex">
+        <div className="border-4 rounded-md border-sky-400">
+          <h3>Discord</h3>
+          <a href="https://discord.gg/EVXYfrgyqh">https://discord.gg/EVXYfrgyqh</a>
+        </div>
       </div>
     </Layout>
   )
