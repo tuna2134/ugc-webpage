22961b52061fc67bba71fb3585231ff65b04d272
mc_fdc <Masato.04.11.2007@gmail.com>
2022-06-10 17:20:13 +0900

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,13 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import Script from 'next/script'
 
 export default function Layout({ children }) {
   return (
     <div>
       <Head>
         <title>union global chat公式サイト</title>
+        <Script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></Script>
       </Head>
       <nav className="bg-violet-500 flex justify-between h-14">
         <Link href="/">
