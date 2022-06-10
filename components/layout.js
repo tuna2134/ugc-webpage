90666d69135505c420b01a33160703f353312fcd
mc_fdc <Masato.04.11.2007@gmail.com>
2022-06-10 18:52:04 +0900

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -7,8 +7,8 @@ export default function Layout({ children }) {
     <div>
       <Head>
         <title>union global chat公式サイト</title>
-        <Script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></Script>
       </Head>
+      <Script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></Script>
       <nav className="bg-violet-500 flex justify-between h-14">
         <Link href="/">
           <h1 className="text-4xl text-white">UGC</h1>
